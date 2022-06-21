43faecd85fab9a7b577ab03d8445f02aca9726fe
Seamus Flannery <seamusmflannery@gmail.com>
2022-06-21 16:50:30 -0400

    added fits to the min max

--- nruhl_final_project/HCNM_Analysis/HC-precision-vs-altitude.py
+++ nruhl_final_project/HCNM_Analysis/HC-precision-vs-altitude.py
@@ -1,6 +1,6 @@
 # Author: Nathaniel Ruhl
 # This script uses the toy model to determine the effects of orbital altitude on the precision of a horizon crossing
-
+from scipy.optimize import curve_fit
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
@@ -21,12 +21,35 @@ hc_type = "rising"
 
 
 def main():
+    test()
+    return 0
+
+
+def test():
     # do_one()
     # do_a_bunch(400, 2100, 25, 100)
     # do_a_bunch_median(400, 2100, 25, 100)
     # plot_a_bunch(400,2100, 25, 100)
-    do_a_bunch_max_min(400, 2100, 25, 100)
-    return 0
+    # print(plot_inverse_root_fit([1, 2, 3, 4], [1, 0.7, 0.57, 0.5]))
+    do_a_bunch_max_min(400, 2100, 25, 50)
+    # print(plot_fit([1, 2, 3], [1, 4, 9], 2))
+
+
+def plot_fit(x, y, degree):  #for polynomial fitting, returns fit data in array
+    fit_params = np.polyfit(x, y, degree)
+    fit = np.poly1d(fit_params)
+    out_array = fit(x)
+    return out_array
+
+
+def sqrt_inverse(x, a):
+    return a/np.sqrt(x)
+
+
+def plot_inverse_root_fit(x, y):
+    params = curve_fit(sqrt_inverse, x, y)
+    out_array = sqrt_inverse(x, params[0])
+    return out_array
 
 
 def do_one():
@@ -148,8 +171,8 @@ def do_a_bunch_max_min(min_alt, max_alt, alt_interval, how_many):
         for i, alt in enumerate(altitude_list):
             sat = AnalyzeCrossing(cb=cb_str, H=alt, E_kev=E_kev)
             comp_obj = CurveComparison(sat, hc_type, N)
-            dt_list[i][j] = comp_obj.dt_e / how_many
-            dr_list[i][j] = comp_obj.dt_e * sat.R_orbit * sat.omega / how_many
+            dt_list[i][j] = comp_obj.dt_e
+            dr_list[i][j] = comp_obj.dt_e * sat.R_orbit * sat.omega
             print("iteration: " + str(j) + " altitude: " + str(alt))
 
     dt_max = np.amax(dt_list, axis=1)
@@ -189,6 +212,84 @@ def do_a_bunch_max_min(min_alt, max_alt, alt_interval, how_many):
     altlist.close()
     return 0
 
+
+def do_a_bunch_max_min(min_alt, max_alt, alt_interval, how_many):
+    altitude_list = np.arange(min_alt, max_alt, alt_interval)
+    dt_list = np.zeros((len(altitude_list),how_many), dtype="float")
+    dr_list = np.zeros((len(altitude_list),how_many), dtype="float")
+    for j in range(how_many):
+        for i, alt in enumerate(altitude_list):
+            sat = AnalyzeCrossing(cb=cb_str, H=alt, E_kev=E_kev)
+            comp_obj = CurveComparison(sat, hc_type, N)
+            dt_list[i][j] = comp_obj.dt_e
+            dr_list[i][j] = comp_obj.dt_e * sat.R_orbit * sat.omega
+            print("iteration: " + str(j) + " altitude: " + str(alt))
+    dt_sort = np.sort(dt_list)
+    dr_sort = np.sort(dr_list)
+    dt_median = np.median(dt_sort, axis=1)
+    dr_median = np.median(dr_sort, axis=1)
+    dt_66 = np.percentile(dt_list, 66, axis=1)
+    dt_33 = np.percentile(dt_list, 33, axis=1)
+    dr_66 = np.percentile(dr_list, 66, axis=1)
+    dr_33 = np.percentile(dr_list, 33, axis=1)
+    dt_medianfit = plot_fit(altitude_list, dt_median, 3)
+    dt_33fit = plot_fit(altitude_list, dt_33, 3)
+    dt_66fit = plot_fit(altitude_list, dt_66, 3)
+    dr_medianfit = plot_fit(altitude_list, dr_median, 3)
+    dr_33fit = plot_fit(altitude_list, dr_33, 3)
+    dr_66fit = plot_fit(altitude_list, dr_66, 3)
+    dt_mediansqfit = plot_inverse_root_fit(altitude_list, dt_median)
+    dt_33sqfit = plot_inverse_root_fit(altitude_list, dt_33)
+    dt_66sqfit = plot_inverse_root_fit(altitude_list, dt_66)
+    dr_mediansqfit = plot_inverse_root_fit(altitude_list, dr_median)
+    dr_33sqfit = plot_inverse_root_fit(altitude_list, dr_33)
+    dr_66sqfit = plot_inverse_root_fit(altitude_list, dr_66)
+
+    plt.figure(1)
+    plt.title(r"$\delta t_e$ uncertainty as a function of orbital altitude")
+    plt.plot(altitude_list, dt_66, label=fr"upper 66 data")
+    plt.plot(altitude_list, dt_33, label=fr"lower 33 data")
+    plt.plot(altitude_list, dt_median, label=fr"median data")
+    plt.plot(altitude_list, dt_66fit, label=fr"upper 66 poly fit")
+    plt.plot(altitude_list, dt_33fit, label=fr"lower 33 poly fit")
+    plt.plot(altitude_list, dt_medianfit, label=fr"median poly fit")
+    plt.plot(altitude_list, dt_66sqfit, label=fr"upper 66  sq-1 fit")
+    plt.plot(altitude_list, dt_33sqfit, label=fr"lower 33 sq-1 fit")
+    plt.plot(altitude_list, dt_mediansqfit, label=fr"median sq-1 fit")
+    plt.ylabel(fr"Temporal uncertaintainty in HCNM meauremental, $\delta t_e$ (sec), {E_kev} keV {cb_str} {hc_type} crossing, $N_0$ = {N}")
+    plt.xlabel("Orbital altitude (km)")
+    plt.legend()
+    plt.savefig("bunches/curvewin_dt_v_alt.png")
+
+    plt.figure(2)
+    plt.title(r"$\delta r_e$ uncertainty as a function of orbital altitude")
+    plt.plot(altitude_list, dr_66, label=fr"upper 66 data")
+    plt.plot(altitude_list, dr_33, label=fr"lower 33 data")
+    plt.plot(altitude_list, dr_median, label=fr"median data")
+    plt.plot(altitude_list, dr_66fit, label=fr"upper 66 poly fit")
+    plt.plot(altitude_list, dr_33fit, label=fr"lower 33 poly fit")
+    plt.plot(altitude_list, dr_medianfit, label=fr"median poly fit")
+    plt.plot(altitude_list, dr_66sqfit, label=fr"upper 66  sq-1 fit")
+    plt.plot(altitude_list, dr_33sqfit, label=fr"lower 33 sq-1 fit")
+    plt.plot(altitude_list, dr_mediansqfit, label=fr"median sq-1 fit")
+    plt.ylabel(r"Positional uncertainty in HCNM measurement, $\delta r_e$ (km)")
+    plt.xlabel("Orbital altitude (km)")
+    plt.savefig("bunches/curvewin_dr_v_alt.png")
+    plt.legend()
+    plt.show()
+
+    dt_data = open("bunches/curvewin_dt_int_" + str(alt_interval) + "_iter_" + str(how_many), "w")
+    dt_data.write(str(dt_list))
+    dt_data.close()
+    dr_data = open("bunches/curvewin_dr_int_" + str(alt_interval) + "_iter_" + str(how_many), "w")
+    dr_data.write(str(dr_list))
+    dr_data.close()
+    altlist = open("bunches/curvewin_alt_int_" + str(alt_interval) + "_iter_" + str(how_many), "w")
+    altlist.write(str(altitude_list))
+    altlist.close()
+    return 0
+
+
 def plot_a_bunch(min_alt, max_alt, alt_interval, how_many):
     altitude_list = np.arange(min_alt, max_alt, alt_interval)
     dt_list = np.zeros((len(altitude_list), how_many), dtype="float")
@@ -197,8 +298,8 @@ def plot_a_bunch(min_alt, max_alt, alt_interval, how_many):
         for i, alt in enumerate(altitude_list):
             sat = AnalyzeCrossing(cb=cb_str, H=alt, E_kev=E_kev)
             comp_obj = CurveComparison(sat, hc_type, N)
-            dt_list[i][j] = comp_obj.dt_e / how_many
-            dr_list[i][j] = comp_obj.dt_e * sat.R_orbit * sat.omega / how_many
+            dt_list[i][j] = comp_obj.dt_e
+            dr_list[i][j] = comp_obj.dt_e * sat.R_orbit * sat.omega
             print("iteration: " + str(j) + " altitude: " + str(alt))
 
     plt.figure(1)
