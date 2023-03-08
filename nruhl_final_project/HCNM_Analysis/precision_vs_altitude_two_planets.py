6357345185a215a65253f6ee616e6f2ed11912d3
Seamus Flannery <seamusmflannery@gmail.com>
2023-03-07 19:39:56 -0500

    Maria Push

--- nruhl_final_project/HCNM_Analysis/precision_vs_altitude_two_planets.py
+++ nruhl_final_project/HCNM_Analysis/precision_vs_altitude_two_planets.py
@@ -137,4 +137,4 @@ def write_data(min_alt, max_alt, alt_interval, how_many):
 
 
 # plot_compare_planets("Earth", "Jupiter", 600, 10000, 100, 100, read=True)  # stable
-plot_compare_planets("Earth", "Jupiter", 600, 10000, 200, 300)  # TODO stabilize on Maria
+plot_compare_planets("Jupiter", "Earth", 600, 10000, 200, 300)  # TODO stabilize on Maria
